fd32134bf9eec42201119b65c83f2b2c65d3830d
KevangPatel <kevangpatel@gmail.com>
2024-04-04 02:55:33 +0530

    Feat: Add see more popup design

--- src/app/components/molecules/event-list-overlay/event-list-overlay.component.ts
+++ src/app/components/molecules/event-list-overlay/event-list-overlay.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { TimelineComponent } from '../../molecules/timeline/timeline.component';
+import { EventCardComponent } from '../../molecules/event-card/event-card.component';
 
 @Component({
   selector: 'app-event-list-overlay',
   standalone: true,
-  imports: [],
+  imports: [EventCardComponent],
   templateUrl: './event-list-overlay.component.html',
   styleUrl: './event-list-overlay.component.scss'
 })
